dce4c22b301cde1575127480967daf4f7e0ea0ca
Alan Neves <alanrenoneves@gmail.com>
2022-08-07 02:30:32 -0300

    feat: add pinia

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,13 +2,15 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { createHead } from '@vueuse/head';
+import { createPinia } from 'pinia';
 import './assets/index.postcss';
 import './assets/css/index.css';
 
 const head = createHead();
+const pinia = createPinia();
 const app = createApp(App);
 
 app.use(router);
 app.use(head);
-
+app.use(pinia);
 app.mount('#app');
